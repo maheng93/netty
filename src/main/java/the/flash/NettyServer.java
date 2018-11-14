12cb2e41e85ae6656f6ace0f2cfe07f2c516fd46
Matthew <maheng93@163.com>
2018-11-14 11:06:57 +0800

    增加第三行
    Signed-off-by: Matthew <maheng93@163.com>

--- src/main/java/the/flash/NettyServer.java
+++ src/main/java/the/flash/NettyServer.java
@@ -45,6 +45,7 @@ public class NettyServer {
                     bind(serverBootstrap, port + 1);
                     System.out.println("1");
                     System.out.println("2");
+                    System.out.println("3");
                 }
             }
         });
